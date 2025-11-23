e4565810b9a6bc4bfe1732f8b457defc8dfcbc97
arthursepp <arthursouzasepp@gmail.com>
2025-11-23 14:55:06 -0300

    fix: Adicionando configurações para deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,23 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  },
+  server: {
+    port: 5173,
+    strictPort: false,
+  },
+  preview: {
+    port: 4173,
+    strictPort: false,
+  }
 })
